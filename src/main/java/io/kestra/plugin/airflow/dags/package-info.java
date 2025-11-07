af965191117b9c24378ad0a26a278f2d3c6989f1
Malay Dewangan <malaydewangan310@gmail.com>
2025-11-07 16:23:48 +0530

    doc: add missing package-info.java

---
+++ src/main/java/io/kestra/plugin/airflow/dags/package-info.java
@@ -0,0 +1,7 @@
+@PluginSubGroup(
+    description = "This sub-group of plugins contains tasks for integrating and triggering Apache Airflow DAGs.",
+    categories = { PluginSubGroup.PluginCategory.OTHER }
+)
+package io.kestra.plugin.airflow.dags;
+
+import io.kestra.core.models.annotations.PluginSubGroup;
