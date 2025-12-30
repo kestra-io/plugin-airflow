81b8f929eb5f6c0efd61bb0f01ef55eaabbd2f4a
ben8t <pimpaudben@gmail.com>
2025-12-30 15:33:53 +0100

    chore: update plugin categories

--- src/main/java/io/kestra/plugin/airflow/dags/package-info.java
+++ src/main/java/io/kestra/plugin/airflow/dags/package-info.java
@@ -1,6 +1,6 @@
 @PluginSubGroup(
     description = "This sub-group of plugins contains tasks for integrating and triggering Apache Airflow DAGs.",
-    categories = { PluginSubGroup.PluginCategory.OTHER }
+        categories = { PluginSubGroup.PluginCategory.DATA }
 )
 package io.kestra.plugin.airflow.dags;
 
